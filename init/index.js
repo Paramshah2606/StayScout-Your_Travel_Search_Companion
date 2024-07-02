af8ec3d7dce99b8f6b1b4840898b3f4fc9114dab
Paramshah2606 <paramshah2606@gmail.com>
2024-07-02 13:17:06 +0530

    bug fix

--- init/index.js
+++ init/index.js
@@ -15,7 +15,6 @@ async function main(){
 }
 
 const initDB=async ()=>{
-    await Listing.deleteMany({});
     initdata.data=initdata.data.map((obj)=>({...obj,owner:"66839569728f924e4880923e",geometry:{ type: 'Point', coordinates: [ 72.579498, 23.02318 ] }}));
     await Listing.insertMany(initdata.data);
     console.log("data was initialized..");
