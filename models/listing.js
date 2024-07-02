84783deadc2877b2bfd4e68097b4b956fa0228a3
Paramshah2606 <paramshah2606@gmail.com>
2024-07-02 13:49:07 +0530

    changes

--- models/listing.js
+++ models/listing.js
@@ -27,11 +27,9 @@ const listingSchema=new Schema({
         type: {
           type: String,
           enum: ['Point'], 
-          required: true
         },
         coordinates: {
           type: [Number],
-          required: true
         }
     }
 });
